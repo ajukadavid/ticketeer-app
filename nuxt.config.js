a9a26b0c00444a3d801caa59b7abbf9076e48de1
ithiel883 <ajuka.zephiniah@gmail.com>
2023-03-19 09:05:48 +0100

    added qr scanner component

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,11 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
+  vue: {
+		compilerOptions: {
+			isCustomElement: (tag) => ['qrcode'].includes(tag)
+		}
+	},
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
