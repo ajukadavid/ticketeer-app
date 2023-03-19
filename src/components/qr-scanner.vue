46f82dc005bed051057fc5e3ee14d95ec7dad7e4
ithiel883 <ajuka.zephiniah@gmail.com>
2023-03-19 07:33:25 +0100

    added qr scanner package

---
+++ src/components/qr-scanner.vue
@@ -0,0 +1,13 @@
+<template>
+<section>
+    <h3>le component</h3>
+</section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <Tutorial/>
+  <QrScanner/>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+    name: "IndexPage",
 })
 </script>
